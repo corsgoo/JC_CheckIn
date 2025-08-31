1cf8706f2cf9465fbfe772dc00edded07d0313b9
corsgoo <170397495+corsgoo@users.noreply.github.com>
2025-08-31 14:53:40 +0800

    Update JC_CheckIn.py

--- JC_CheckIn.py
+++ JC_CheckIn.py
@@ -1,10 +1,15 @@
+
+
 import requests
 import json
 import os
+##  环境变量需要 SCKEY   EMAIL_PASSWORD 账号格式为email&password ，多账号为email&password#email&password#email&password   BASE_URL这个是机场地址 比如：https://78cloud.pro，sckey是pushplus的推送token。
 requests.packages.urllib3.disable_warnings()
-SCKEY = os.environ['SCKEY']
-def checkin(email=os.environ['EMAIL'], password=os.environ['PASSWORD'],
-            base_url=os.environ['BASE_URL']):
+SCKEY = os.environ['JCSCKEY']
+EMAIL_PASSWORD = os.environ['JCEMAIL_PASSWORD'] # 用#分隔多个账号密码
+BASE_URL = os.environ['JCBASE_URL']
+
+def checkin(email, password, base_url, account_num):
     email = email.split('@')
     email = email[0] + '%40' + email[1]
     session = requests.session()
@@ -28,10 +33,17 @@ def checkin(email=os.environ['EMAIL'], password=os.environ['PASSWORD'],
     response = session.post(base_url + '/user/checkin', headers=headers,
                             verify=False)
     response = json.loads(response.text)
-    print(response['msg'])
+    print(f'账号{account_num}: {response["msg"]}')
     return response['msg']
 
 
-if SCKEY != '':
-    sendurl = 'http://www.pushplus.plus/send?token=' + SCKEY + '&title=机场签到&content=' + checkin()
-    r = requests.get(url=sendurl)
+if __name__ == "__main__":
+    account_list = EMAIL_PASSWORD.split('#')  # 多账号用#做分割
+    num_accounts = len(account_list)  # 获取账号数量
+    result = []
+    for i, account in enumerate(account_list):
+        email, password = account.split('&')  # 账号格式为 email&password
+        result.append(checkin(email, password, BASE_URL, i + 1))
+    if SCKEY != '':
+        sendurl = 'http://www.pushplus.plus/send?token=' + SCKEY + '&title=机场签到&content=' + '\n'.join(result)
+        r = requests.get(url=sendurl)
